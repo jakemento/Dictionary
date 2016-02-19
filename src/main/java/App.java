a039d1a3bcac72dfcf79ef1fa6e6979ee7606d18
Jake Steinberg <student@epicodus.com>
2016-02-19 11:32:49 -0800

    set the index to display my dictionary words

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,44 +12,35 @@ public class App {
     get("/", (request,response) -> {
       HashMap<String,Object> model = new HashMap<String,Object>();
       model.put("template", "templates/index.vtl");
+      model.put("words", Word.all());
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/words/new", (request,response) -> {
+    get("/new", (request,response) -> {
       HashMap<String,Object> model = new HashMap<String,Object>();
       model.put("template", "templates/word-form.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/words", (request,response) -> {
+    post("/", (request,response) -> {
       HashMap<String,Object> model = new HashMap<String,Object>();
       String wordName = request.queryParams("wordName");
       Word newWord = new Word(wordName);
       model.put("words", Word.all());
-      model.put("template", "templates/words.vtl");
+      model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/words", (request,response) -> {
+    get("/:id", (request,response) -> {
       HashMap<String,Object> model = new HashMap<String,Object>();
-
-      model.put("words", Word.all());
-
-      model.put("template", "templates/words.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    get("/words/:id", (request,response) -> {
-      HashMap<String,Object> model = new HashMap<String,Object>();
-
       Word word = Word.find(Integer.parseInt(request.params(":id")));
       model.put("word", word);
-
       model.put("template", "templates/word.vtl");
+
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/words/:id/definitions/new", (request,response) -> {
+    get("/:id/definitions/new", (request,response) -> {
       HashMap<String,Object> model = new HashMap<String,Object>();
 
       Word word = Word.find(Integer.parseInt(request.params(":id")));
